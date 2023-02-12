75c2b8b824fd0d39126abe43911e6df2afbbb748
Marvin Ambrose <marvambi@gmail.com>
2023-02-12 03:15:15 +0100

    fix: update for backend mongodb url with no .evn

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ import path from 'path';
 dotenv.config();
 
 const HOST = process.env.HOST || 'http://localhost';
-const PORT = parseInt(process.env.PORT || '4500');
+const PORT = parseInt(process.env.PORT || '80');
 
 const app = express();
 const corsOptions = {
   // eslint-disable-next-line max-len
-  origin: ['http://localhost:8081', 'http://localhost:3000', 'https://63e7bddb54858c000859a043--darling-rolypoly-632b11.netlify.app/'],
+  origin: ['http://localhost:3000', 'https://63e7bddb54858c000859a043--darling-rolypoly-632b11.netlify.app/'],
   credentials: true,
 };
 
